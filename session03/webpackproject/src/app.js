5727757490b3d1245e5d4d720129d4a5f5110683
Christian Wahl <chrwahl@gmail.com>
2021-04-08 10:34:03 +0200

    webpack example

---
+++ session03/webpackproject/src/app.js
@@ -0,0 +1,3 @@
+import './scss/style.scss';
+import './js/hover.js';
+import './js/click.js';
---
+++ session03/webpackproject/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        // Find either sass or scss files and load them
+        // using style-loader, css-loader and sass-loader
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ],
+      },
+    ],
+  }
+};
